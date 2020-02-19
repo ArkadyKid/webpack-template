2aed6c54555e48ce8dee8845a3d172cb759ee490
ArkadyKid <kidanov.a.y1986@gmail.com>
2020-02-19 19:57:31 +0300

    test

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,1 @@
-import { includes } from 'lodash';
-import "../css/style.css";
-// import assert from 'power-assert';
-
-const arr = [1, 2, 6, 4];
-const includesArr = includes(arr, 2);
-assert(includesArr(arr, 2) === 3);
+export default str => str.split('').reverse().join('');
--- src/js/index.test.js
+++
@@ -1,6 +0,0 @@
-import assert from 'power-assert';
-import includesArr from './index';
-
-const arr = [1, 2, 6, 4];
-assert(includesArr(arr, 2) === 3);
-console.log('all done');
